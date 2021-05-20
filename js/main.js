7e1929ed154269805d2a171cb35918cf0301c2e0
杨高飞 <yanggf@seeyon.com>
2021-05-20 16:26:05 +0800

    up

--- js/main.js
+++ js/main.js
@@ -2,50 +2,17 @@
 
   var schemaData = {};
 
-  function postData(url, data) {
-    // Default options are marked with *
-    return fetch(url, {
-      body: JSON.stringify(data), // must match 'Content-Type' header
-      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: 'same-origin', // include, same-origin, *omit
-      headers: {
-        'user-agent': 'Mozilla/4.0 MDN Example',
-        'content-type': 'application/json'
-      },
-      method: 'POST', // *GET, POST, PUT, DELETE, etc.
-      mode: 'cors', // no-cors, cors, *same-origin
-      redirect: 'follow', // manual, *follow, error
-      referrer: 'no-referrer', // *client, no-referrer
-    })
-    .then(response => response.text()) // parses response to JSON
-  }
-
   function update() {
-
     var input = editor.getValue();
-    // try {
-    //   var fn = ejs.compile(input, {
-    //     client: true,
-    //     rmWhitespace: true,
-    //     fileName: 'parseNode',
-    //   });
-    //   r = fn(schemaData, null, (path, d) => {
-    //     return parseNode({
-    //       nodes: d
-    //     })
-    //   });
-
-    // } catch (e) {
-    //   r = e.stack;
-    //   throw(e);
-    // }
-
-    postData('/compile', {
-      input,
-    }).then(r => {
-      console.log(r)
-      result.setValue(js_beautify(r.replace(/\n+/g, '\n')), -1);
-    })
+    var r = "";
+    try {
+      r = ejs.render(input, schemaData, {
+        rmWhitespace: true, 
+      })
+    } catch (e) {
+      r = e.stack;
+    }
+    result.setValue(js_beautify(r.replace(/\n+/g, '\n')), -1);
   }
 
   function updateSchema() {
@@ -79,29 +46,18 @@
     enableLiveAutocompletion: true
   });
 
-  fetch(`./parseNode.ejs?v=${Math.random()}`, {
+
+
+  fetch(`./parse.ejs?v=${Math.random()}`, {
     headers: {
       'content-type': 'text/plain'
     }
   }).then(res => res.text()).then(res => {
-    // window.parseNode = res;
-    window.parseNode= ejs.compile(res, {
-      rmWhitespace: true,
-      // client: true,
-      fileName: 'parseNode',
-      cache: true,
-    });
+    editor.setValue(res, -1);
+    editor.focus();
+  });
 
-    fetch(`./parse.ejs?v=${Math.random()}`, {
-      headers: {
-        'content-type': 'text/plain'
-      }
-    }).then(res => res.text()).then(res => {
-      editor.setValue(res, -1);
-      editor.focus();
-    });
 
-  })
 
 
 
